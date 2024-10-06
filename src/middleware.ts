e842719633e629d27407e5478bd59b233036abb2
Álvaro Peixoto <alvaro.peixoto@dcomp.ufs.br>
2024-10-06 18:07:38 -0300

    feat: change middleware logics

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,31 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { validateSpotifyToken } from "./lib/spotify";
 
-export function middleware(request: NextRequest) {
-  const token = request.cookies.get('access_token');
+export async function middleware(request: NextRequest) {
+  const accessToken = request.cookies.get('access_token')?.value;
+  const refreshToken = request.cookies.get('refresh_token')?.value;
+  const tokenExpiration = request.cookies.get('token_expiration')?.value;
 
-  if (!token) {
+  if (!accessToken || !refreshToken || !tokenExpiration) {
     return NextResponse.redirect(new URL("/api/spotify/login", request.url));
+  }
+
+  if (Date.now() > Number(tokenExpiration)) {
+    try {
+      const { accessToken: newAccessToken, expirationTime } = await validateSpotifyToken(accessToken, refreshToken);
+
+      if (newAccessToken && expirationTime) {
+        const response = NextResponse.next();
+        response.cookies.set('access_token', newAccessToken, { httpOnly: true, path: '/', maxAge: 60 * 60 * 24 * 30 });
+        response.cookies.set('token_expiration', expirationTime.toString(), { httpOnly: true, path: '/', maxAge: 60 * 60 * 24 * 30 });
+        return response;
+      }
+    } catch (error) {
+      console.error('Failed to refresh token', error);
+
+      return NextResponse.redirect(new URL("/api/spotify/login", request.url));
+    }
   }
 
   return NextResponse.next();
